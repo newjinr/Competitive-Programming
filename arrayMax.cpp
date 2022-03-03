ca72d537e2788aeeb6b7742b75b01e7e1063c525
Newjin <programmernewjin@gmail.com>
2022-03-03 21:22:16 +0530

    Array Maximum

--- arrayMax.cpp
+++ arrayMax.cpp
@@ -1,42 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
+#define ull unsigned long long
 
 int main()
 {
-    // write your code here
+    // your code goes here
     int t;
     cin >> t;
     while (t--)
     {
         int n, k;
         cin >> n >> k;
-        ll *arr = new ll[k]{0};
-        ll A[n], ans = 0;
+        int maximum = 0;
+        int A[n];
+        ull *ans = new ull[k]{0};
+        ll values[k]{0};
         for (int i = 0; i < n; ++i)
         {
             cin >> A[i];
+            if (maximum < A[i])
+                maximum = A[i];
             if (i < k)
-                arr[i] = A[i];
+                values[i] = A[i];
         }
 
-        for (int i = 0; i < n - k; ++i)
+        for (int i = k; i < n; i++)
         {
-            ll num = arr[i % k];
-            ll sum = A[i + k] + num;
-            arr[i % k] = sum;
+            if (values[i % k] < 0)
+                values[i % k] = A[i];
+            else
+            {
+                values[i % k] += A[i];
+                if (values[i % k] > 0 && ans[i % k] < values[i % k])
+                    ans[i % k] = values[i % k];
+            }
         }
 
-        sort(arr, arr + k);
+        sort(ans, ans + k);
 
-        if (arr[k - 1] > ans)
-            ans = arr[k - 1];
-
-        for (int i = n - k; i < n; ++i)
-            if (A[i] > ans)
-                ans = A[i];
-        cout << ans << endl;
+        if (ans[k - 1] > maximum)
+            cout << ans[k - 1] << endl;
+        else
+            cout << maximum << endl;
     }
-
     return 0;
 }
